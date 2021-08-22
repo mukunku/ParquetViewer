3ea67696688f9b463ccd563fb24761ef4666e4d6
Sal <chunkymonkey_42@hotmail.com>
2021-08-22 08:49:19 -0400

    - add arrow:schema support. - move metadata stuff to its own project to reduce package pollution on main project

--- src/ParquetFileViewer/MetadataViewer.cs
+++ src/ParquetFileViewer/MetadataViewer.cs
@@ -1,12 +1,8 @@
-﻿using Parquet.Thrift;
-using ParquetFileViewer.Helpers;
-using System;
-using System.Linq;
+﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Utilities;
 
 namespace ParquetFileViewer
 {
@@ -58,34 +54,8 @@ namespace ParquetFileViewer
             var metadataResult = new List<(string TabName, string Text)>();
             if (parquetReader.ThriftMetadata != null)
             {
-                var thriftMetadata = parquetReader.ThriftMetadata;
-                var jsonObject = new Newtonsoft.Json.Linq.JObject();
-                jsonObject[nameof(thriftMetadata.Version)] = thriftMetadata.Version;
-                jsonObject[nameof(thriftMetadata.Num_rows)] = thriftMetadata.Num_rows;
-                jsonObject[nameof(thriftMetadata.Created_by)] = thriftMetadata.Created_by;
-
-                var schemas = new Newtonsoft.Json.Linq.JArray();
-                foreach (var schema in thriftMetadata.Schema)
-                {
-                    if ("schema".Equals(schema.Name) && schemas.Count == 0)
-                        continue;
-
-                    var schemaObject = new Newtonsoft.Json.Linq.JObject();
-                    schemaObject[nameof(schema.Field_id)] = schema.Field_id;
-                    schemaObject[nameof(schema.Name)] = schema.Name;
-                    schemaObject[nameof(schema.Type)] = schema.Type.ToString();
-                    schemaObject[nameof(schema.Type_length)] = schema.Type_length;
-                    schemaObject[nameof(schema.LogicalType)] = schema.LogicalType?.ToString();
-                    schemaObject[nameof(schema.Scale)] = schema.Scale;
-                    schemaObject[nameof(schema.Precision)] = schema.Precision;
-                    schemaObject[nameof(schema.Repetition_type)] = schema.Repetition_type.ToString();
-                    schemaObject[nameof(schema.Converted_type)] = schema.Converted_type.ToString();
-
-                    schemas.Add(schemaObject);
-                }
-                jsonObject[nameof(thriftMetadata.Schema)] = schemas;
-
-                metadataResult.Add((THRIFT_METADATA, jsonObject.ToString().FormatJSON()));
+                string json = ParquetMetadataAnalyzers.ThriftMetadataToJSON(parquetReader.ThriftMetadata);
+                metadataResult.Add((THRIFT_METADATA, json));
             }
             else
                 metadataResult.Add((THRIFT_METADATA, "No thrift metadata available"));
@@ -97,14 +67,11 @@ namespace ParquetFileViewer
                     string value = _customMetadata.Value;
                     if (PANDAS_SCHEMA.Equals(_customMetadata.Key))
                     {
-                        value = value.FormatJSON();
+                        value = ParquetMetadataAnalyzers.PandasSchemaToJSON(value);
                     }
                     else if (APACHE_ARROW_SCHEMA.Equals(_customMetadata.Key))
                     {
-                        //TODO: Base64 decode on its own doesn't accomplish anything.
-                        //Need some way to read the schema but there isn't anything in the apache arrow repo for this...
-                        //https://github.com/apache/arrow/blob/master/csharp/src/Apache.Arrow/Ipc/MessageSerializer.cs
-                        //value = value.Base64Decode();
+                        value = ParquetMetadataAnalyzers.ApacheArrowToJSON(value);
                     }
 
                     metadataResult.Add((_customMetadata.Key, value));
