e780b0c925c2b6fdbe70f4b4aaeca920af2970a5
Sal <chunkymonkey_42@hotmail.com>
2022-08-12 23:53:49 -0400

    cleanup

--- src/ParquetFileViewer/Helpers/CustomScriptBasedSchemaAdapter.cs
+++ src/ParquetFileViewer/Helpers/CustomScriptBasedSchemaAdapter.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.Data;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace ParquetFileViewer.Helpers
 {
