854bb0dced7e531c25aa0adb53ee801db3c4a0b7
Sal <chunkymonkey_42@hotmail.com>
2023-08-30 11:52:54 -0400

    Add "SC" suffix to version number in about box for self contained deployments

--- src/ParquetViewer/AboutBox.cs
+++ src/ParquetViewer/AboutBox.cs
@@ -6,12 +6,17 @@ namespace ParquetViewer
 {
     public partial class AboutBox : Form
     {
+#if RELEASE_SELFCONTAINED
+        private bool _isSelfContainedExe = true;
+#else
+        private bool _isSelfContainedExe = false;
+#endif
         public AboutBox()
         {
             InitializeComponent();
             this.Text = String.Format("About {0}", AssemblyTitle);
             this.labelProductName.Text = AssemblyProduct;
-            this.labelVersion.Text = String.Format("Version {0}", AssemblyVersion);
+            this.labelVersion.Text = String.Format("Version {0}{1}", AssemblyVersion, _isSelfContainedExe ? " SC" : string.Empty);
             this.labelCopyright.Text = AssemblyCopyright;
             this.labelCompanyName.Text = AssemblyCompany;
             this.textBoxDescription.Text = AssemblyDescription;
