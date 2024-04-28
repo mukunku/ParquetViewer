47f8bd209a32d00203e7a9b9fe4737816502c138
Sal <chunkymonkey_42@hotmail.com>
2024-04-28 18:22:04 -0400

    remove using

--- src/ParquetViewer/AboutBox.cs
+++ src/ParquetViewer/AboutBox.cs
@@ -5,7 +5,6 @@ using System;
 using System.Diagnostics;
 using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Security.Principal;
 using System.Text;
 using System.Windows.Forms;
 
