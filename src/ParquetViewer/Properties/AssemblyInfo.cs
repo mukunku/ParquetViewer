3c8990d9c893771cb51c20c4e2a9c40dbf5627ae
Sal <chunkymonkey_42@hotmail.com>
2026-01-11 16:37:02 -0500

    more cleanup

--- src/ParquetViewer/Properties/AssemblyInfo.cs
+++ src/ParquetViewer/Properties/AssemblyInfo.cs
@@ -2,9 +2,6 @@
 using System.Runtime.InteropServices;
 using System.Runtime.Versioning;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("ParquetViewer")]
 [assembly: AssemblyDescription("Simple Windows desktop application for viewing Apache Parquet files\r\n" +
     "https://github.com/mukunku/ParquetViewer\r\n\r\n" +
@@ -16,23 +13,12 @@ using System.Runtime.Versioning;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: SupportedOSPlatform("windows")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("134e9435-2fb2-4297-ac49-d44477a427bc")]
 
 // Version information for an assembly consists of the following four values:
-//
 //      Major Version
 //      Minor Version 
-//      Build Number
+//      Patch Version
 //      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("4.0.0.0")]
